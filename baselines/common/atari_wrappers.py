dbcb75e47f128fe6936a9d94b0723fdaea7bd649
simontudo <simon.hakenes@tu-dortmund.de>
2018-01-31 11:52:28 +0100

    solved dtype issue

--- baselines/common/atari_wrappers.py
+++ baselines/common/atari_wrappers.py
@@ -136,7 +136,9 @@ class WarpFrame(gym.ObservationWrapper):
         self.width = 84
         self.height = 84
         self.observation_space = spaces.Box(low=0, high=255,
-            shape=(self.height, self.width, 1), dtype=np.uint8)
+            shape=(self.height, self.width, 1))
+            #shape=(self.height, self.width, 1), dtype=np.uint8)
+
 
     def observation(self, frame):
         frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
@@ -157,7 +159,9 @@ class FrameStack(gym.Wrapper):
         self.k = k
         self.frames = deque([], maxlen=k)
         shp = env.observation_space.shape
-        self.observation_space = spaces.Box(low=0, high=255, shape=(shp[0], shp[1], shp[2] * k), dtype=np.uint8)
+        self.observation_space = spaces.Box(low=0, high=255, shape=(shp[0], shp[1], shp[2] * k))
+        #self.observation_space = spaces.Box(low=0, high=255, shape=(shp[0], shp[1], shp[2] * k), dtype=np.uint8)
+
 
     def reset(self):
         ob = self.env.reset()
